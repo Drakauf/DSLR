c56d3839f8103bafb3599cee076da59a95f2299d
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-03-08 23:29:24 +0100

    updateObj

--- LIB/ft_tools.py
+++ LIB/ft_tools.py
@@ -1,4 +1,6 @@
+import sys
 import numpy as np
+from collections import Iterable
 from LIB.ft_maths import ft_count, ft_mean, ft_std, ft_min, ft_max, ft_percentile
 
 def fillRet(data, retD):
@@ -22,3 +24,19 @@ def fillRet(data, retD):
         except :
             notNum.append(data[i][0])
     return(notNum)
+
+def updateObj(dest, src):
+    if not type(dest) is dict or not type(src) is dict:
+        sys.exit("updateObj : one of your parameter is not a dict")
+    for key in src:
+        if isinstance(src[key], Iterable):
+            for elem in src[key]:
+                try:
+                    dest[key].append(elem)
+                except:
+                    dest[key] = [elem]
+        else:
+            try:
+                dest[key] = [dest[key], src[key]]
+            except:
+                dest[key] = src[key]
--- histogram.py
+++ histogram.py
@@ -1,6 +1,6 @@
 import sys
 from LIB.ft_parse import readData, retData
-from LIB.ft_tools import fillRet
+from LIB.ft_tools import fillRet, updateObj
 from LIB.ft_print import printNotNum, printRet
 import numpy as np
 import csv
@@ -21,25 +21,19 @@ if __name__ == "__main__":
     dictt = {
             'sex':['male','female','female'],
             'survived': [0,1,1],
-            'class':['one','two','three']
+            'class':['one','two','three'],
             }
     dictt2 = {
             'sex':['male','female','female'],
             'se':['male','female','female'],
             'survived': [3,1,3],
-            'class':['o','o','o']
+            'class':['o','o','o'],
+            'test': 3
             }
-    for key in dictt2:
-        print(key)
-        for elem in dictt2[key]:
-            print(elem)
-            try:
-                dictt[key].append(elem)
-            except:
-                dictt[key] = [elem]
+    updateObj(dictt, dictt2)
     print(dictt)
-    df = pd.DataFrame(dictt, columns = ['sex', 'survived', 'class'])
-    print(df)
-    sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=df)
-    sns.set(style="darkgrid")
-    plt.show()
+    #df = pd.DataFrame(dictt, columns = ['sex', 'survived', 'class'])
+    #print(df)
+    #sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=df)
+    #sns.set(style="darkgrid")
+    #plt.show()
