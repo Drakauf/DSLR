42da771457ddd9bcfe19db1446d414b235bae3e1
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-03-08 18:04:34 +0100

    clean

--- descripe.py
+++ descripe.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     if (lArg != 2 and not verbose):
         sys.exit("Wrong usage: python3 describe.py filename [-v]");
     data = readData(sys.argv[1]);
-    retD = retData(["", "Count","Mean","np_Mean","Std","np_Std","Min","np_Min","25%","np_25","50%","np_50","75%","np_75","Max","np_Max"])
+    retD = retData(["", "Count","Mean","Std","Min","25%","50%","75%", "Max"])
     notNum = fillRet(data, retD)
     if verbose:
         printNotNum(notNum)
