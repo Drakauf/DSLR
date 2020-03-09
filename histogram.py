40ec6a037ccb2f91a067bb63ea7830b8895ac7ba
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-03-09 22:30:51 +0100

    Loop over tab and return marks and nb data

--- histogram.py
+++ histogram.py
@@ -9,6 +9,21 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+def getData(house, marks):
+    ret = dict()
+    i = 0
+    for nb in marks:
+        if not np.isnan(nb):
+            try:
+                ret[house[i]]["total"] += marks[i]
+                ret[house[i]]["nb"] += 1
+            except:
+                ret[house[i]] = dict()
+                ret[house[i]]["total"] = marks[i]
+                ret[house[i]]["nb"] = 1
+        i += 1
+    return(ret)
+
 if __name__ == "__main__":
     verbose = 0
     lArg = len(sys.argv);
@@ -18,6 +33,10 @@ if __name__ == "__main__":
         sys.exit("Wrong usage: python3 describe.py filename [-v]");
     data = readData(sys.argv[1])
     
+    d = ["a", "b", "c", "d", "a"]
+    d2 = np.array([1, 3, 5, np.nan, 1.2], dtype=float)
+
+
     dictt = {
             'sex':['male','female','female'],
             'survived': [0,1,1],
@@ -31,7 +50,7 @@ if __name__ == "__main__":
             'test': 3
             }
     updateObj(dictt, dictt2)
-    print(dictt)
+    print(getData(d,d2))
     #df = pd.DataFrame(dictt, columns = ['sex', 'survived', 'class'])
     #print(df)
     #sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=df)
