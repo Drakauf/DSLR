2492604c03d5ddbc382bc7e69626cbfa8f3c9288
Shankithan Thevakumar <shthevak@student.le-101.fr>
2020-03-10 14:55:13 +0100

    Continuying histogram

--- histogram.py
+++ histogram.py
@@ -9,20 +9,40 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def getData(house, marks):
+def getData(house, marks, feature):
     ret = dict()
     i = 0
+    j = 0
+    retFormat = dict()
+    retFormat["Feature"] = [feature]
+    retFormat["Marks"] = [0]
+    retFormat["House"] = ["School"]
     for nb in marks:
         if not np.isnan(nb):
             try:
                 ret[house[i]]["total"] += marks[i]
                 ret[house[i]]["nb"] += 1
+                retFormat["Marks"][0] += marks[i]
+                j +=1
             except:
                 ret[house[i]] = dict()
                 ret[house[i]]["total"] = marks[i]
                 ret[house[i]]["nb"] = 1
+                retFormat["Marks"][0] += marks[i]
+                j +=1
         i += 1
-    return(ret)
+    retFormat["Marks"][0] = retFormat["Marks"][0] / j
+    for elem in ret:
+        try:
+            retFormat["Feature"].append(feature)
+            retFormat["Marks"].append(ret[elem]["total"] / ret[elem]["nb"])
+            retFormat["House"].append(elem)
+        except:
+            print(marks[0])
+            retFormat["Feature"] = [feature]
+            retFormat["Marks"] = [ret[elem]["total"] / ret[elem]["nb"]]
+            retFormat["House"] = [elem]
+    return(retFormat)
 
 if __name__ == "__main__":
     verbose = 0
@@ -35,7 +55,6 @@ if __name__ == "__main__":
     
     d = ["a", "b", "c", "d", "a"]
     d2 = np.array([1, 3, 5, np.nan, 1.2], dtype=float)
-
 
     dictt = {
             'sex':['male','female','female'],
@@ -50,9 +69,20 @@ if __name__ == "__main__":
             'test': 3
             }
     updateObj(dictt, dictt2)
-    print(getData(d,d2))
-    #df = pd.DataFrame(dictt, columns = ['sex', 'survived', 'class'])
-    #print(df)
-    #sns.catplot(x="sex", y="survived", hue="class", kind="bar", data=df)
-    #sns.set(style="darkgrid")
-    #plt.show()
+    test = getData(data[1][1:],np.array(data[6][1:], dtype=float), data[6][0])
+    test["Feature"].append("Arithman")
+    test["Marks"].append(490)
+    test["House"].append("School")
+    
+    test["Feature"].append("Arithman")
+    test["Marks"].append(49)
+    test["House"].append("Slytherin")
+
+    df = pd.DataFrame(test , columns = ['Feature','Marks','House'])
+    sns.catplot(x="Feature", y="Marks", hue="House", kind="bar", data=df)
+    sns.set(style="darkgrid")
+    plt.title('Histogram')
+    plt.show()
+    print(df)
+
+
